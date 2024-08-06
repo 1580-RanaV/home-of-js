d653e5a7dcbcfa65289acf558630d8b1fb553f2b
1580-RanaV <rviyyapu@gitam.in>
2024-08-06 22:31:32 +0530

    first-commit

---
+++ data.js
@@ -0,0 +1,51 @@
+export const stories = [
+    {
+        profileImg: "profile.jpg",
+        profileName: "Pixel5",
+        title: [
+            "Summary of my",
+            "works with",
+            "Pixel5 studio.",
+        ],
+        linkLabel: "Click to see more",
+        linkSrc: "./general.html",
+        storyImg: "slide3.png",
+    },
+    {
+        profileImg: "profile.jpg",
+        profileName: "Adidas",
+        title: [
+            "Take a",
+            "look at some of my works",
+            "for Adidas.",
+        ],
+        linkLabel: "Click to see more",
+        linkSrc: "./adidas.html",
+        storyImg: "slide2.png",
+    },
+    {
+        profileImg: "profile.jpg",
+        profileName: "BuildNext",
+        title: [
+            "Peek some",
+            "works done at BuildNext",
+            "Studio.",
+        ],
+        linkLabel: "Click to see more",
+        linkSrc: "./buildnext.html",
+        storyImg: "slide4.png",
+    },
+    {
+        profileImg: "profile.jpg",
+        profileName: "Myself",
+        title: [
+            "Get to know more about",
+            "V Jaya Surya",
+            "and his come up.",
+        ],
+        linkLabel: "Click to see more",
+        linkSrc: "./about.html",
+        storyImg: "slide1.png",
+    }
+    
+]
